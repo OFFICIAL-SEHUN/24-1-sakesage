897e749803b63b58cd3f207bc7d655692cd94810
OFFICIAL-SEHUN <j48750@daum.net>
2024-03-29 18:17:20 +0900

    Revert "login_ui_ver2"
    This reverts commit 704f1ceabe64ad3183eb2a56ec1d80e43a1467c2.

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -20,61 +20,49 @@ class _LoginScreenState extends State<LoginScreen> {
       resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(36.0),
+          padding: const EdgeInsets.all(48.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset("assets/sakesage.png"),
               const SizedBox(
-                height: 30,
+                height: 64,
               ),
               Form(
                 child: Column(
-                  children: [
-                    Container(
-                      height: 80, // 이메일 입력칸의 높이 조정
-                      child: TextFormField(
-                        controller: emailTextContoller,
-                        style: TextStyle(fontSize: 18), // 폰트 크기 조정
-                        decoration: InputDecoration(
-                          border: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey), // underline color
-                          ),
-                          labelText: "이메일",
-                          hintText: "ex) abc@sakesage.co.kr",
-                        ),
-                        validator: (value){
-                          if (value == null || value.isEmpty){
-                            return "이메일 주소를 입력하세요.";
-                          }
-                          return null;
-                        },
-                      ),
+                children: [
+                  TextFormField(
+                    controller: emailTextContoller,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "이메일",
                     ),
-                    //const SizedBox(height: 24,),
-                    Container(
-                      height : 80,
-                      child: TextFormField(
-                        controller: pwdTextContoller,
-                        style: TextStyle(fontSize: 18), // 폰트 크기 조정
-                        decoration: InputDecoration(
-                          border: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey), // underline color
-                          ),
-                          labelText: "비밀번호",
-                        ),
-                        obscureText: true,
-                        keyboardType: TextInputType.visiblePassword,
-                        validator: (value){
-                          if (value == null || value.isEmpty){
-                            return "비밀번호를 입력하세요.";
-                          }
-                          return null;
-                        },
-                      ),
+                    validator: (value){
+                      if (value == null || value.isEmpty){
+                        return "이메일 주소를 입력하세요.";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 24,
+                  ),
+                  TextFormField(
+                    controller: pwdTextContoller,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "비밀번호",
                     ),
-                  ],
-                ),
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                    validator: (value){
+                      if (value == null || value.isEmpty){
+                        return "비밀번호를 입력하세요.";
+                      }
+                      return null;
+                    },
+                  )
+                ],
+              ),
               ),
               Padding(
                 padding: const EdgeInsets.only(top: 16),
@@ -91,22 +79,15 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ),
               TextButton(
-                onPressed: (){},
-                child: const Text("계정이 없나요? 회원가입"),
+                  onPressed: (){},
+                  child: const Text("계정이 없나요? 회원가입"),
               ),
               const Divider(),
-              Image.asset("assets/naver_login_icon.png",
-                width: 60,
-                height: 60),
-
-              Image.asset("assets/google_login_icon.png",
-                width: 60,
-                height: 60,)
+              Image.asset("assets/btn_google_signin.png")
             ],
           ),
         ),
       ),
     );
   }
-
 }
