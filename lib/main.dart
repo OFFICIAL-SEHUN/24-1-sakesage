afe898e463f9978d9813d96e5f1dc9f1a9d99641
OFFICIAL-SEHUN <j48750@daum.net>
2024-04-05 14:25:22 +0900

    0405_ver2

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: LoginScreen(),
+      home: HomeScreen(),
       //HomeScreen(),
       //SignUpScreen(),
       //LoginScreen(),
