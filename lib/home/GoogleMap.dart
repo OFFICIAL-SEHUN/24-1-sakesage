f8d8bc6939d528acb664c086a166c6106a4a44b1
OFFICIAL-SEHUN <j48750@daum.net>
2024-05-08 20:50:05 +0900

    google-map-fixed

--- lib/home/GoogleMap.dart
+++ lib/home/GoogleMap.dart
@@ -3,13 +3,14 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 class GoogleMapScreen extends StatefulWidget {
+  const GoogleMapScreen({Key?key}):super(key:key);
   @override
   _GoogleMapScreenState createState() => _GoogleMapScreenState();
 }
 
 class _GoogleMapScreenState extends State<GoogleMapScreen> {
   late GoogleMapController mapController;
-  final LatLng _center = const LatLng(37.50508097213444, 126.95493073306663); // 초기 중심 좌표
+  final LatLng _center = const LatLng(37.7749, -122.4194); // 초기 중심 좌표
   final Set<Marker> _markers = {}; // 마커 집합
 
   // 지도가 준비되면 호출되는 콜백 함수
@@ -34,13 +35,14 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Google Map'),
+        title: Text('Sake Store Map'),
+        backgroundColor: Colors.lightBlueAccent[100],
       ),
       body: GoogleMap(
         onMapCreated: _onMapCreated,
         initialCameraPosition: CameraPosition(
           target: _center,
-          zoom: 18.0, // 초기 줌 레벨
+          zoom: 11.0, // 초기 줌 레벨
         ),
         markers: _markers,
       ),
